8910aeb68a91379b4de38a19a38ab60983038781
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2023-08-09 11:39:36 +0530

    Create Table.cpp

---
+++ Table.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int n,i;
+    i=1;
+    cout<<"\nEnter the number :";
+    cin>>n;
+
+    while(i<=10)
+    {
+        cout<<n<<"*"<<i<<"="<<n*i<<endl;
+        i++;
+    }
+}
+
+// 2*1=2
+// 2*2=4
+// 2*3=6
+..........
+  
