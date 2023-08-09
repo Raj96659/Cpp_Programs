a63e2eb0f5041039532a7553b3c2cc4a2f92ade2
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2023-08-09 09:32:46 +0530

    Update fibonnacci.cpp

--- fibonnacci.cpp
+++ fibonnacci.cpp
@@ -1,3 +1,6 @@
+// Fibonacci sequence is a sequence in which each number is the sum of the two preceding ones.
+// 0, 1, 1, 2, 3, 5, 8, 13, 21 ............
+
 #include<iostream>
 using namespace std;
 main()
